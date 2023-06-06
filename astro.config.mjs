eeee4571386d78155c82e117fbb06f75d4a578d5
CDzungx <59353870+CDzungx@users.noreply.github.com>
2023-06-06 13:07:17 +0700

    Init 1
    Add modules, simple components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import solidJs from "@astrojs/solid-js";
+import sitemap from "@astrojs/sitemap";
+import image from "@astrojs/image";
+import compress from "astro-compress";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    solidJs(),
+    sitemap(),
+    image({
+      cacheDir: "./.cache/image",
+    }),
+    compress()
+  ],
+});
