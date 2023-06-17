42a7efc81bcd1ec76a4dd307fe3d569ce4edd1a6
CDzungx <59353870+CDzungx@users.noreply.github.com>
2023-06-18 01:07:38 +0700

    Fix 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,39 +2,30 @@ import { defineConfig } from "astro/config";
 import prefetch from "@astrojs/prefetch";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
-import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
 import alpinejs from "@astrojs/alpinejs";
 import markdoc from "@astrojs/markdoc";
 import vercel from "@astrojs/vercel/serverless";
 import compress from "astro-compress";
-import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
-
   build: {
     inlineStylesheets: "auto",
   },
   ssr: {
-    noExternal: ['@motionone/solid', '@solid-primitives/props'],
+    noExternal: ["@motionone/solid", "@solid-primitives/props"],
   },
   integrations: [
     prefetch(),
     tailwind(),
     solidJs(),
+    image({ cacheDir: "./.cache/image", serviceEntryPoint: "@astrojs/image/sharp" }),
     sitemap(),
-    image({
-      cacheDir: "./.cache/image",
-    }),
     alpinejs(),
     markdoc(),
     compress(),
-    purgecss({
-      fontFace: true,
-      keyframes: true,
-      rejected: true,
-    }),
   ],
   output: "hybrid",
   adapter: vercel({
